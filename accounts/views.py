ebf08a2e6200aee1570fefd3f34274087699bb52
NoSayMe <gabriel.juraj@gmail.com>
2019-01-22 06:25:58 +0100

    [add] Post model

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.getusername(request.POST['username'])
+                user = User.objects.get(username=request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error':'Username already taken'})
             except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
